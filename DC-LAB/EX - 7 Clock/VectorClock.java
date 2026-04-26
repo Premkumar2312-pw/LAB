1984dd67930d73ca3f24efb971f169851c0f043b
Prem Kumar M <premkumarm23122005@gmail.com>
2026-04-26 08:48:27 +0530

    Refactor VectorClock for dynamic process count

--- DC-LAB/EX - 7 Clock/VectorClock.java
+++ DC-LAB/EX - 7 Clock/VectorClock.java
@@ -2,21 +2,25 @@ import java.util.Scanner;
 
 public class VectorClock {
 
-    static final int MAX_P = 10;
-    static final int MAX_E = 20;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int P;
-        int[] events = new int[MAX_P];
-        int[][][] clock = new int[MAX_P][MAX_E][MAX_P];
 
         System.out.print("Enter number of processes: ");
-        P = sc.nextInt();
-        // Initialization
+        int P = sc.nextInt();
+
+        int[] events = new int[P];
+        int maxE = 0;
+
         for (int i = 0; i < P; i++) {
             System.out.print("Events for P" + (i + 1) + ": ");
             events[i] = sc.nextInt();
+            if (events[i] > maxE) maxE = events[i];
+        }
 
+        int[][][] clock = new int[P][maxE][P];
+
+        // Initialization
+        for (int i = 0; i < P; i++) {
             clock[i][0][i] = 1;
 
             for (int j = 1; j < events[i]; j++) {
@@ -26,7 +30,8 @@ public class VectorClock {
                 clock[i][j][i]++;
             }
         }
-        // ✅ Initial Vector Clocks
+
+        // Initial display
         System.out.println("\nInitial Vector Clocks:");
         for (int i = 0; i < P; i++) {
             System.out.println("P" + (i + 1) + ":");
@@ -38,25 +43,25 @@ public class VectorClock {
             }
         }
 
-        int msg;
         System.out.print("\nEnter number of messages: ");
-        msg = sc.nextInt();
+        int msg = sc.nextInt();
+
         for (int m = 0; m < msg; m++) {
-            int sp, se, rp, re;
             System.out.println("\nMessage " + (m + 1));
+
             System.out.print("Sender (process event): ");
-            sp = sc.nextInt() - 1;
-            se = sc.nextInt() - 1;
+            int sp = sc.nextInt() - 1;
+            int se = sc.nextInt() - 1;
 
             System.out.print("Receiver (process event): ");
-            rp = sc.nextInt() - 1;
-            re = sc.nextInt() - 1;
-            // Receive rule
+            int rp = sc.nextInt() - 1;
+            int re = sc.nextInt() - 1;
+
             for (int k = 0; k < P; k++)
                 clock[rp][re][k] = Math.max(clock[rp][re][k], clock[sp][se][k]);
 
             clock[rp][re][rp]++;
-            // Propagation
+
             for (int j = re + 1; j < events[rp]; j++) {
                 for (int k = 0; k < P; k++)
                     clock[rp][j][k] = clock[rp][j - 1][k];
@@ -64,7 +69,8 @@ public class VectorClock {
                 clock[rp][j][rp]++;
             }
         }
-        // Final Output
+
+        // Final output
         System.out.println("\nFinal Vector Clocks:");
         for (int i = 0; i < P; i++) {
             System.out.println("P" + (i + 1) + ":");
