a8fc303faae4f0da65ef2231b053ef992f66050c
Prem Kumar M <premkumarm23122005@gmail.com>
2026-04-26 06:10:11 -0400

    Update RMI server to connect to existing registry

--- DC-LAB/EX - 6 RMI/Server.java
+++ DC-LAB/EX - 6 RMI/Server.java
@@ -4,10 +4,13 @@ import java.rmi.registry.Registry;
 public class Server {
     public static void main(String[] args) {
         try {
+            // Create service object
             AddService service = new AddServiceImpl();
 
-            Registry registry = LocateRegistry.createRegistry(1099);
+            // Connect to already running registry
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
 
+            // Bind service
             registry.rebind("CalcService", service);
 
             System.out.println("RMI Calculator Server is running...");
