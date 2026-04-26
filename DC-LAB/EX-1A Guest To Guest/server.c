e0692876acd1592cbf14d4e37c48581308eb39bf
Prem Kumar M <premkumarm23122005@gmail.com>
2026-04-26 04:45:54 -0400

    Refactor server.c for improved readability and structure

--- DC-LAB/EX-1A Guest To Guest/server.c
+++ DC-LAB/EX-1A Guest To Guest/server.c
@@ -3,43 +3,50 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+
 #define PORT 8080
+
 int main() {
     int server_fd, client_socket;
     struct sockaddr_in server_addr;
     char buffer[1024];
 
-    // Create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Configure server address
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    // Bind
     bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-    // Listen
     listen(server_fd, 1);
 
-    printf("Chat Server started on port %d\n", PORT);
-    printf("Server IP: ");
-    system("hostname -I | awk '{print $1}'");
+    printf("Server started...\n");
 
-    // Accept client
     client_socket = accept(server_fd, NULL, NULL);
     printf("Client connected\n");
 
     while (1) {
-        // Receive message
+
+        // RECEIVE
         memset(buffer, 0, sizeof(buffer));
         read(client_socket, buffer, sizeof(buffer));
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("Client disconnected\n");
+            break;
+        }
+
         printf("Client: %s", buffer);
-      
-        // Send reply
+
+        // SEND
         printf("You: ");
         fgets(buffer, sizeof(buffer), stdin);
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            write(client_socket, buffer, strlen(buffer));
+            break;
+        }
+
         write(client_socket, buffer, strlen(buffer));
     }
 
